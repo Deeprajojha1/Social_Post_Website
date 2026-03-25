f62af43a41cdebecf6cf7825916f8584e270c5b2
Deepraj <deeprajojha2022@gmail.com>
2026-03-25 16:14:08 +0530

    add the backend hosted uri

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-  import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+  import.meta.env.VITE_API_URL ||"http://localhost:3000/api";
 
 export const api = axios.create({
   baseURL,
